7427b6e09c3325d375947202366ac10ad9caa711
Annie <mimeow@gmail.com>
2022-04-12 13:16:15 +0800

    v1

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import re
+
 def main():
     val = input("輸入要查詢的單詞")
-    arr = re.findall(r'(\d[A-z])',val)
-    for e in arr:
-        print(e)
+    reg = "^"
+    for e in list(val):
+        for j in list(e):
+            if re.match(r'[A-z]', j):
+                reg += j
+            elif re.match(r'\d', j):
+                reg += "[A-z]{"+j+"}"
+    reg += "$"
+    f = open('words.txt')
+    b = open("ans.txt","w")
+    b.write("")
+    b = open("ans.txt","a")
+    line = f.readline()
+    while line:
+        if re.match(rf"{reg}",line):
+            print(line)
+            b.write(line)
+        line = f.readline()
+    f.close()
 main()
